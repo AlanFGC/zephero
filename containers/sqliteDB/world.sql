dac4e02410654f50415fb1eebf3f7111c93ce9ea
Alan Garcia <alan_fgc@outlook.com>
2024-08-14 01:03:44 -0700

    Added save and load chunks

--- containers/sqliteDB/world.sql
+++ containers/sqliteDB/world.sql
@@ -4,8 +4,8 @@ CREATE TABLE world_chunk (
   world_id INTEGER,
   row_id INTEGER,
   col_id INTEGER,
-  PRIMARY KEY (world_id, row_id, col_id),
   last_updated DATETIME DEFAULT CURRENT_TIMESTAMP,
   locked BOOLEAN,
-  data chunk NOT NULL
+  data chunk NOT NULL,
+  PRIMARY KEY (world_id, row_id, col_id)
 );
