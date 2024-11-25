4e2999e7a0a234e8a988893230f5989b7705b5ce
Alan Garcia <alan_fgc@outlook.com>
2024-11-24 20:42:28 -0800

    Create game access manager with preload function

---
+++ server/worldAccess.go
@@ -0,0 +1,82 @@
+package server
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	worldRepo "zephero/database/sqlite_world_repo"
+	gameWorld "zephero/shared"
+)
+
+type WorldAccess struct {
+	World   *gameWorld.ChunkedWorld
+	worldId int64
+}
+
+// TODO it's probably a better idea for this function to recieve a new world, instead of instantiating it
+func (wa *WorldAccess) Preload(ctx context.Context, path string, worldId int) error {
+	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	worldQueries := worldRepo.New(db)
+	sqlWorld, err := worldQueries.GetWorld(ctx, int64(worldId))
+	if err != nil {
+		return err
+	}
+
+	world, err := gameWorld.NewChunkedWorld(int(sqlWorld.RowLength),
+		int(sqlWorld.ColumnLength),
+		int(sqlWorld.ChunkLength))
+	if err != nil || world == nil {
+		return err
+	}
+	wa.World = world
+
+	sqlChunks, err := worldQueries.GetWorldChunkByWorldId(ctx, int64(worldId))
+	if err != nil {
+		return err
+	}
+
+	if len(sqlChunks) != int(sqlWorld.RowLength*sqlWorld.ColumnLength) {
+		return fmt.Errorf("world %d has %d chunks", worldId, len(sqlChunks))
+	}
+
+	for i := 0; i < len(sqlChunks); i++ {
+		chunk, err := gameWorld.DeserializeChunkData(sqlChunks[i].Chunk)
+		if err != nil {
+			return err
+		}
+		err = world.SetChunk(int(sqlChunks[i].RowID), int(sqlChunks[i].ColID), chunk)
+		if err != nil {
+			return err
+		}
+	}
+	log.Println(fmt.Sprint("Successfully preloaded world ", worldId))
+	return nil
+}
+
+func (wa *WorldAccess) Save(ctx context.Context, path string) error {
+	if wa.World == nil {
+		return fmt.Errorf("Error: world is nil")
+	}
+	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	// TODO implement this
+}
